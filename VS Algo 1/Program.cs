42fb730c0a32236fc966afb4687ef8cc03cbed9e
ikaatc <ika.tania.ft21@mail.umy.ac.id>
2022-10-06 23:10:11 +0700

    Membuat method untuk mengurutkan algoritma menggunakan bubble sort

--- VS Algo 1/Program.cs
+++ VS Algo 1/Program.cs
@@ -50,5 +50,23 @@ namespace Bubble_Sort
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n - 1 passes
+            {
+                // Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) // Jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
